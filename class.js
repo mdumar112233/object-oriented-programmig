6e2105cec21a8590d8627fdae40755d985fa85c2
md umar faruk <mdumar11233>
2021-04-23 23:05:29 +0600

    object oriented programming

---
+++ class.js
@@ -0,0 +1,17 @@
+class PenDrive {
+    constructor(capacity, color, price){
+        this.capacity = capacity;
+        this.color = color;
+        this.price = price;
+    }
+}
+
+
+const ignite = new PenDrive('16gb', 'white', 322);
+const tohsiba = new PenDrive('4gb', 'red', 322);
+console.log(ignite);
+console.log(tohsiba);
+
+
+
+
